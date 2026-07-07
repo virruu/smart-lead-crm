f75b70b2748e1a723b93a1a69a7f6e46538de8ff
virruu <150152091+virruu@users.noreply.github.com>
2026-07-07 15:51:38 +0530

    Bump plugin version to 1.4.0

--- smart-lead-crm.php
+++ smart-lead-crm.php
@@ -22,8 +22,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Plugin constants.
  */
-define( 'SMART_LEAD_CRM_VERSION', '1.3.0' );
-define( 'SMART_LEAD_CRM_DB_VERSION', '1.3.0' );
+define( 'SMART_LEAD_CRM_VERSION', '1.4.0' );
+define( 'SMART_LEAD_CRM_DB_VERSION', '1.4.0' );
 define( 'SMART_LEAD_CRM_PLUGIN_FILE', __FILE__ );
 define( 'SMART_LEAD_CRM_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'SMART_LEAD_CRM_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
